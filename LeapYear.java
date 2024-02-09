7db430a52a72063895bfa96ae19d1173edbd1b3b
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2024-02-09 11:15:03 +0530

    LeapYear

---
+++ LeapYear.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = scanner.nextInt();
+        scanner.close();
+
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0)
+            isLeapYear = true;
+        }
+
+        if(isLeapYear)
+        System.out.println(year + " is a leap year.");
+        else
+        System.out.println(year + " is not a leap year.");
+    }
+}
