b9291cc7f77a610ea4f0dbc7478f8b8ded3ccb29
Govinda Prasad Parida <62826820+govinda006@users.noreply.github.com>
2024-02-09 11:31:26 +0530

    factorial

--- Factorial.java
+++ Factorial.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int num = scanner.nextInt();
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int num = scanner.nextInt();
+            scanner.close();
 
-        int factorial = 1;
-        for(int i = 1; i <= num; i++){
-        factorial *= i;
+            int factorial = 1;
+            for(int i = 1; i <= num; i++){
+            factorial *= i;
+            }
+
+            System.out.println("Factorial of " + num + " is " + factorial);
         }
-
-        System.out.println("Factorial of " + num + " is " + factorial);
     }
 }
